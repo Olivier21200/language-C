37935a4150bc0553a96d34518a55c483b7752009
Olivier Sirugue <sirugue.e2203896@etud.univ-ubs.fr>
2023-05-19 18:00:21 +0200

    Update Autour_du_cercle.c

--- langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP1/src/Autour_du_cercle.c
+++ langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP1/src/Autour_du_cercle.c
@@ -9,13 +9,16 @@ int main()
 {
     float rayon, perimetre, surface;
     char input[MAX_INPUT];
-    
 
     printf("Entrez la valeur du rayon du cercle en cm: ");
     fgets(input, sizeof(input), stdin); //évite les dépassements de mémoire
     rayon = strtof(input, NULL);//interpréter une valeur à virgule flottante dans une chaîne d'octets de manière sécurisée évite les injections
-    // Test si le rayon est négatif
-    if (rayon < 0) 
+    if (rayon == 0 && strcmp(input, "0") != 0) // Vérifie si la conversion a échoué
+    {
+        printf("Entrée invalide.\n");
+        return 1;
+    }
+    if (rayon < 0)  // Test si le rayon est négatif
     {
         printf("Le rayon ne peut pas être négatif.\n");
     }
