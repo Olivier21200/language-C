a4e5b31caec6100f4ca3f25f90c5da8c8580c2e4
Olivier Sirugue <sirugue.e2203896@etud.univ-ubs.fr>
2023-05-21 17:17:16 +0200

    Update TD3

--- langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP3/src/Conversions_degres/testdegre2.c
+++ langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP3/src/Conversions_degres/testdegre2.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "degre.h"
 
 int lireChoix() 
 {
     int choix;
-    
+    char input[10];
+
     printf("Choisissez une conversion :\n");
     printf("1 - Celsius vers Kelvin\n");
     printf("2 - Celsius vers Fahrenheit\n");
     printf("0 - Quitter\n");
-    
-    scanf("%d", &choix);
+
+    fgets(input, sizeof(input), stdin);
+    sscanf(input, "%d", &choix);
+
     return choix;
 }
 
@@ -18,19 +23,21 @@ void executerChoix(int choix)
 {
     double tempCelsius;
     double tempConvertie;
-    
-    printf("Entrez la temperature a convertir en degres Celsius : ");
-    scanf("%lf", &tempCelsius);
+    char input[20];
+
+    printf("Entrez la température à convertir en degrés Celsius : ");
+    fgets(input, sizeof(input), stdin);
+    sscanf(input, "%lf", &tempCelsius);
     
     switch (choix) 
     {
         case 1:
             tempConvertie = CelsiusAKelvin(tempCelsius);
-            printf("%.1f degres Celsius = %.1f degres Kelvin\n", tempCelsius, tempConvertie);
+            printf("%.1f degrés Celsius = %.1f degrés Kelvin\n", tempCelsius, tempConvertie);
             break;
         case 2:
             tempConvertie = CelsiusAFahrenheit(tempCelsius);
-            printf("%.1f degres Celsius = %.1f degres Fahrenheit\n", tempCelsius, tempConvertie);
+            printf("%.1f degrés Celsius = %.1f degrés Fahrenheit\n", tempCelsius, tempConvertie);
             break;
         default:
             printf("Choix invalide.\n");
