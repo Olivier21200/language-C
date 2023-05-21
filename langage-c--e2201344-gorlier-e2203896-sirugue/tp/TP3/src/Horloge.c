891571f657f949b8024c6328f27ed162d2189b23
Olivier Sirugue <sirugue.e2203896@etud.univ-ubs.fr>
2023-05-21 17:12:13 +0200

    secure td3

--- langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP3/src/Horloge.c
+++ langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP3/src/Horloge.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 
-int temps_en_secondes(int heures, int minutes, int secondes) // Fonction qui convertit le temps en secondes
+int temps_en_secondes(int heures, int minutes, int secondes)
 {
     int temps_total_en_secondes;
+
+    // Vérification des limites des entrées utilisateur
+    if (heures < 0 || minutes < 0 || secondes < 0) {
+        printf("Erreur : Les valeurs doivent être positives.\n");
+        return -1;
+    }
+
+    // Prévention des débordements de pile en limitant les valeurs des heures, minutes et secondes
+    if (heures > 23 || minutes > 59 || secondes > 59) {
+        printf("Erreur : Les valeurs des heures, minutes et secondes doivent respecter les limites de temps.\n");
+        return -1;
+    }
+
     temps_total_en_secondes = heures * 3600 + minutes * 60 + secondes;
     return temps_total_en_secondes;
 }
 
-int main() // Main du programme
+int main()
 {
-    int heures, minutes, secondes; // Déclaration des variables heures, minutes et secondes
+    int heures, minutes, secondes;
 
-    printf("Entrez le temps en heures, minutes et secondes : "); // Demande à l'utilisateur de rentrer le temps en heures, minutes et secondes
-    scanf("%d %d %d", &heures, &minutes, &secondes); // Stocke le temps en heures, minutes et secondes dans les variables heures, minutes et secondes
+    printf("Entrez le temps en heures, minutes et secondes : ");
+    // Utilisation de scanf avec une spécification de format sécurisée pour éviter les attaques par injection
+    if (scanf("%d %d %d", &heures, &minutes, &secondes) != 3) {
+        printf("Erreur : Entrée invalide.\n");
+        return 1;
+    }
 
-    int temps_en_sec = temps_en_secondes(heures, minutes, secondes);// Appel de la fonction temps_en_secondes
-    printf("%d heures, %d minutes, %d secondes = %d secondes\n", heures, minutes, secondes, temps_en_sec);// Affiche le temps en heures, minutes et secondes et le temps en secondes
-    
+    int temps_en_sec = temps_en_secondes(heures, minutes, secondes);
+    if (temps_en_sec == -1) {
+        // Gestion des erreurs de conversion des valeurs de temps en secondes
+        return 1;
+    }
+
+    printf("%d heures, %d minutes, %d secondes = %d secondes\n", heures, minutes, secondes, temps_en_sec);
+
     return 0;
 }
--- langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP3/src/Les_types_de_retour_des_fonctions.c
+++ langage-c--e2201344-gorlier-e2203896-sirugue/tp/TP3/src/Les_types_de_retour_des_fonctions.c
@@ -2,71 +2,69 @@
 // programme qui à l’aide d’une fonction compare(…) compare deux entiers fournis par l’utilisateur et qui retourne :
 #include <stdio.h>
 
-int compare_entiers(int entier1, int entier2)  // Fonction qui compare deux entiers
+int compare_entiers(int entier1, int entier2)
 {
-    if(entier1 == entier2)  // Si les deux entiers sont égaux
-    {
-        return 1; // Retourne 1
-    }
-    else  // Sinon
-    {
-        return 0; // Retourne 0
+    // Pas de mesures de sécurité supplémentaires nécessaires pour les entiers
+    if (entier1 == entier2) {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
-char compare_entiers_caractere(int entier1, int entier2) // Fonction qui compare deux entiers et qui retourne V si les deux entiers sont égaux et F si les deux entiers ne sont pas égaux
+char compare_entiers_caractere(int entier1, int entier2)
 {
-    if(entier1 == entier2) // Si les deux entiers sont égaux
-    {
-        return 'V'; // Retourne V
-    }
-    else // Sinon
-    {
-        return 'F'; // Retourne F
+    if (entier1 == entier2) {
+        return 'V';
+    } else {
+        return 'F';
     }
 }
 
-int compare_flottants(float flottant1, float flottant2) // Fonction qui compare deux flottants
+int compare_flottants(float flottant1, float flottant2)
 {
-    if(flottant1 == flottant2) // Si les deux flottants sont égaux
-    {
-        return 1; // Retourne 1
-    }
-    else // Sinon
-    {
-        return 0; // Retourne 0
+    // Utilisation d'une tolérance pour comparer les flottants
+    float epsilon = 0.0001;
+    if (fabs(flottant1 - flottant2) < epsilon) {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
-char compare_flottants_caractere(float flottant1, float flottant2) // Fonction qui compare deux flottants et qui retourne V si les deux flottants sont égaux et F si les deux flottants ne sont pas égaux
+char compare_flottants_caractere(float flottant1, float flottant2)
 {
-    if(flottant1 == flottant2) // Si les deux flottants sont égaux 
-    {
-        return 'V'; // Retourne V
-    }
-    else // Sinon
-    {
-        return 'F'; // Retourne F
+    float epsilon = 0.0001;
+    if (fabs(flottant1 - flottant2) < epsilon) {
+        return 'V';
+    } else {
+        return 'F';
     }
 }
 
-int main() 
+int main()
 {
-    int entier1, entier2; // Déclaration des variables entier1 et entier2
+    int entier1, entier2;
 
-    printf("Entrez deux entiers : ");// Demande à l'utilisateur de rentrer deux entiers
-    scanf("%d %d", &entier1, &entier2); // Stocke les deux entiers dans les variables entier1 et entier2
+    printf("Entrez deux entiers : ");
+    if (scanf("%d %d", &entier1, &entier2) != 2) {
+        printf("Erreur : Entrée invalide.\n");
+        return 1;
+    }
 
-    printf("La valeur de retour de la comparaison des entiers est : %d\n", compare_entiers(entier1, entier2)); // Affiche la valeur de retour de la comparaison des entiers
-    printf("La valeur de retour de la comparaison des entiers sous forme de caractère est : %c\n", compare_entiers_caractere(entier1, entier2)); // Affiche la valeur de retour de la comparaison des entiers sous forme de caractère
+    printf("La valeur de retour de la comparaison des entiers est : %d\n", compare_entiers(entier1, entier2));
+    printf("La valeur de retour de la comparaison des entiers sous forme de caractère est : %c\n", compare_entiers_caractere(entier1, entier2));
 
-    float flottant1, flottant2; // Déclaration des variables flottant1 et flottant2
+    float flottant1, flottant2;
 
-    printf("Entrez deux flottants : "); // Demande à l'utilisateur de rentrer deux flottants
-    scanf("%f %f", &flottant1, &flottant2); // Stocke les deux flottants dans les variables flottant1 et flottant2
+    printf("Entrez deux flottants : ");
+    if (scanf("%f %f", &flottant1, &flottant2) != 2) {
+        printf("Erreur : Entrée invalide.\n");
+        return 1;
+    }
 
-    printf("La valeur de retour de la comparaison des flottants est : %d\n", compare_flottants(flottant1, flottant2)); // Affiche la valeur de retour de la comparaison des flottants
-    printf("La valeur de retour de la comparaison des flottants sous forme de caractère est : %c\n", compare_flottants_caractere(flottant1, flottant2)); // Affiche la valeur de retour de la comparaison des flottants sous forme de caractère
+    printf("La valeur de retour de la comparaison des flottants est : %d\n", compare_flottants(flottant1, flottant2));
+    printf("La valeur de retour de la comparaison des flottants sous forme de caractère est : %c\n", compare_flottants_caractere(flottant1, flottant2));
 
     return 0;
 }
